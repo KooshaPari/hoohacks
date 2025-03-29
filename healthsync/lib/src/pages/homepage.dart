d0c39599d1fab76f972df7eff7fdb3ee2ed5a315
Smyan Sengupta <senguptasmyan@gmail.com>
2025-03-29 18:57:15 -0400

    moved insights to top of homepage

--- healthsync/lib/src/pages/homepage.dart
+++ healthsync/lib/src/pages/homepage.dart
@@ -22,6 +22,46 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              // const SizedBox(height: 8),
+              Card(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.insights,
+                            size: 40,
+                            color: Colors.purple,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            'Your Monthly Insights',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               SizedBox(height: 20),
               Text(
                 'At a Glance',
@@ -63,45 +103,6 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.blue,
               ),
               SizedBox(height: 20),
-              Card(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: const Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.insights,
-                            size: 40,
-                            color: Colors.purple,
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            'Your Monthly Insights',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              )
             ],
           ),
         ),
