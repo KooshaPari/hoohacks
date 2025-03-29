9bfded24d1b426bf25c518ea1c017449b5b24b85
Koosha Paridehpour <kooshapari@gmail.com>
2025-03-29 12:43:48 -0400

    Initial Commit

---
+++ HoohacksTests/HoohacksTests.swift
@@ -0,0 +1,16 @@
+//
+//  HoohacksTests.swift
+//  HoohacksTests
+//
+//  Created by Koosha Paridehpour on 3/29/25.
+//
+
+import Testing
+
+struct HoohacksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
