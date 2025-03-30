4287e48e218d9a573eff796cdcc75d69224d5d9b
Smyan Sengupta <senguptasmyan@gmail.com>
2025-03-29 22:06:37 -0400

    cleaned up weekly summary data

--- healthsync/lib/src/pages/weekly_summary.dart
+++ healthsync/lib/src/pages/weekly_summary.dart
@@ -8,10 +8,26 @@ class WeeklySummary extends StatefulWidget {
   State<WeeklySummary> createState() => _WeeklySummaryState();
 }
 
+enum HealthData {
+  sleep,
+  calories,
+  steps,
+  heartRate,
+}
+
+// get a graph for each type of data
+// TODO: make a way to update this/integrate with DB
+Map<HealthData, List<double>> healthData = {
+  HealthData.sleep: [6.5, 7.0, 8.0, 5.5, 6.0, 7.5, 10.0],
+  HealthData.calories: [450, 520, 600, 480, 500, 550, 700],
+  HealthData.steps: [6840, 5930, 10500, 8550, 7200, 9000, 12000],
+  HealthData.heartRate: [72, 75, 80, 68, 70, 78, 85],
+};
+
 class _WeeklySummaryState extends State<WeeklySummary> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(8.0),
@@ -20,22 +36,22 @@ class _WeeklySummaryState extends State<WeeklySummary> {
             children: <Widget>[
               WeeklyGraph(
                 title: 'Sleep',
-                values: [6.5, 7.0, 8.0, 5.5, 6.0, 7.5, 10.0],
+                values: healthData[HealthData.sleep]!,
                 color: Colors.lightBlue,
               ),
               WeeklyGraph(
                 title: 'Calories',
-                values: [450, 520, 600, 480, 500, 550, 700],
+                values: healthData[HealthData.calories]!,
                 color: Colors.red,
               ),
               WeeklyGraph(
                 title: 'Steps',
-                values: [6840, 5930, 10500, 8550, 7200, 9000, 12000],
+                values: healthData[HealthData.steps]!,
                 color: Colors.orange,
               ),
               WeeklyGraph(
                 title: 'Heart Rate',
-                values: [72, 75, 80, 68, 70, 78, 85],
+                values: healthData[HealthData.heartRate]!,
                 color: Colors.purple,
               ),
             ],
